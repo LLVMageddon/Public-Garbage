26dacc2103a90dbc86f06f63815b63e43287b84d
Andre <144938604+LLVMageddon@users.noreply.github.com>
2025-12-10 19:02:58 +0200

    First Commit

---
+++ ComHostSettings.js
@@ -0,0 +1,6 @@
+//THis is the best five lines of code I've ever written. 
+$(document).ready(function() {
+    $("#SettingsComitButton").off('click').on('click', function() {
+        alert("Fuck This");
+    });
+});
