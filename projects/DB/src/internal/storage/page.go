34abca8731e715b6a99ff08f9798d7fb502906b1
Andre <144938604+LLVMageddon@users.noreply.github.com>
2026-01-06 01:38:16 +0200

    some integration tests

--- projects/DB/src/internal/storage/page.go
+++ projects/DB/src/internal/storage/page.go
@@ -6,61 +6,60 @@ import (
 	"hash/crc32"
 )
 
-//These datatypes and sizes are copied from PostgreSQL
-const(
+// These datatypes and sizes are copied from PostgreSQL
+const (
 	PageSize = 8192
 	// PageSize = 4096
 	PageHeaderSize = 32
 
-	PageMagic uint32 = 0xDBDBDBDB 
+	PageMagic uint32 = 0xDBDBDBDB
 )
 
-var(
-	ErrInvalidPageSize = errors.New("invalid page size")
-	ErrInvalidMagic = errors.New("invalid page magic")
+var (
+	ErrInvalidPageSize  = errors.New("invalid page size")
+	ErrInvalidMagic     = errors.New("invalid page magic")
 	ErrChecksumMismatch = errors.New("checksum mismatch")
 )
 
-type PageHeader struct{
-	Magic uint32	// 04 bytes	//	Magic Number at begginning of page header
-	Version uint16	// 02 bytes //	Version for forward and backwards compatiablity
-	Flags uint16	// 02 bytes // 	Page Tyep flag
-	PageID uint64	// 08 bytes // 	Page Id
+type PageHeader struct {
+	Magic    uint32 // 04 bytes	//	Magic Number at begginning of page header
+	Version  uint16 // 02 bytes //	Version for forward and backwards compatiablity
+	Flags    uint16 // 02 bytes // 	Page Tyep flag
+	PageID   uint64 // 08 bytes // 	Page Id
 	DataSize uint32 // 04 bytes //	Bytes Used
 	Checksum uint32 // 04 bytes //	CRC error detection
 	Reserved uint64 // 08 bytes //	Special Space
 }
 
 const (
-	OffsetMagic 	= 0
-	OffsetVersion 	= 4
-	OffsetFlags 	= 6
-	OffsetPageID 	= 8
-	OffsetDataSize	= 16
-	OffsetChecksum	= 20
-	OffsetReserved	= 24
-	OffsetData		= 32
+	OffsetMagic    = 0
+	OffsetVersion  = 4
+	OffsetFlags    = 6
+	OffsetPageID   = 8
+	OffsetDataSize = 16
+	OffsetChecksum = 20
+	OffsetReserved = 24
+	OffsetData     = 32
 )
 
-
-type Page struct{
-	Header PageHeader 	// 32 bytes 
-	Data	[]byte		// 8160 bytes 	
+type Page struct {
+	Header PageHeader // 32 bytes
+	Data   []byte     // 8160 bytes
 }
 
-func NewPage(pageId uint64) *Page{
+func NewPage(pageId uint64) *Page {
 	newPage := &Page{
 		Header: PageHeader{
-			Magic: PageMagic,
+			Magic:   PageMagic,
 			Version: 1,
-			PageID : pageId,
+			PageID:  pageId,
 		},
 		Data: make([]byte, PageSize-PageHeaderSize),
 	}
 	return newPage
 }
 
-func serializeHelper(page *Page, checksum uint32)([]byte, error){
+func serializeHelper(page *Page, checksum uint32) ([]byte, error) {
 	buf := make([]byte, PageSize)
 
 	binary.LittleEndian.PutUint32(buf[OffsetMagic:], page.Header.Magic)
@@ -76,8 +75,8 @@ func serializeHelper(page *Page, checksum uint32)([]byte, error){
 	return buf, nil
 }
 
-func (page *Page) Serialize()([]byte, error){
-	if len(page.Data) != PageSize-PageHeaderSize{
+func (page *Page) Serialize() ([]byte, error) {
+	if len(page.Data) != PageSize-PageHeaderSize {
 		return nil, ErrInvalidPageSize
 	}
 
@@ -86,41 +85,39 @@ func (page *Page) Serialize()([]byte, error){
 	return buf, nil
 }
 
-func Deserialize(raw []byte) (*Page, error){
-	if len(raw) != PageSize{
+func Deserialize(raw []byte) (*Page, error) {
+	if len(raw) != PageSize {
 		return nil, ErrInvalidPageSize
 	}
 
 	magic := binary.LittleEndian.Uint32(raw[OffsetMagic:])
-	if magic != PageMagic{
+	if magic != PageMagic {
 		return nil, ErrInvalidMagic
 	}
 
 	checksum := binary.LittleEndian.Uint32(raw[OffsetChecksum:])
-	binary.LittleEndian.PutUint32(raw[OffsetChecksum:],0)
+	binary.LittleEndian.PutUint32(raw[OffsetChecksum:], 0)
 	calculatedChecksum := crc32.ChecksumIEEE(raw)
 
-	if checksum != calculatedChecksum{
+	if checksum != calculatedChecksum {
 		return nil, ErrChecksumMismatch
 	}
 
-
 	header := PageHeader{
-		Magic: magic,
-		Version: binary.LittleEndian.Uint16(raw[OffsetVersion:]),
-		Flags: binary.LittleEndian.Uint16(raw[OffsetFlags:]),
-		PageID: binary.LittleEndian.Uint64(raw[OffsetPageID:]),
+		Magic:    magic,
+		Version:  binary.LittleEndian.Uint16(raw[OffsetVersion:]),
+		Flags:    binary.LittleEndian.Uint16(raw[OffsetFlags:]),
+		PageID:   binary.LittleEndian.Uint64(raw[OffsetPageID:]),
 		DataSize: binary.LittleEndian.Uint32(raw[OffsetDataSize:]),
 		Checksum: checksum,
 		Reserved: binary.LittleEndian.Uint64(raw[OffsetReserved:]),
 	}
 
-
 	data := make([]byte, PageSize-PageHeaderSize)
 	copy(data, raw[OffsetData:])
 	page := &Page{
 		Header: header,
-		Data: data,
+		Data:   data,
 	}
 
 	return page, nil
--- projects/DB/src/tests/integration/page_disk_manager_v1_test.go
+++ projects/DB/src/tests/integration/page_disk_manager_v1_test.go
@@ -4,6 +4,7 @@ import (
 	"dbp/src/internal/storage"
 	"dbp/src/internal/storage/disk"
 	"encoding/binary"
+	"fmt"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -85,7 +86,7 @@ func TestSinglePageToFile(t *testing.T) {
 
 	dm, err := disk.OpenDiskManager(path, storage.PageSize)
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Open Disk Manager error: %v", err)
 	}
 
 	defer dm.Close()
@@ -100,47 +101,225 @@ func TestSinglePageToFile(t *testing.T) {
 	page := makeTestPage(rows)
 	pid, err := dm.AllocatePage()
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Allocating Page error: %v", err)
 	}
 
 	page.Header.PageID = uint64(pid)
 
 	buf, err := page.Serialize()
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Serialize error: %v", err)
 	}
 
 	err = dm.WritePage(pid, buf)
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Write Page error: %v", err)
 	}
 
 	err = dm.Sync()
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Sync error: %v", err)
 	}
 
 	readBuf := make([]byte, storage.PageSize)
 	err = dm.ReadPage(pid, readBuf)
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Read Page error: %v", err)
 	}
 
 	readPage, err := storage.Deserialize(readBuf)
 	if err != nil {
-		t.Fatalf("TestingSinglePageToFile error: %v", err)
+		t.Fatalf("TestingSinglePageToFile: Deserialize error: %v", err)
 	}
 
 	for i, r := range rows {
 		start := i * RowSize
 		got := decodeRow(readPage.Data[start : start+RowSize])
-
-		if r.ID != got.ID || r.Username != got.Username || r.Password != got.Password {
-
-			// if r != got{
-			t.Fatalf("TestingSinglePageToFile error: %v", err)
+		if r != got {
+			t.Fatalf("TestingSinglePageToFile: Row Values error")
 		}
 
+	}
+
+}
+
+func TestMultiplePageSingleFile(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "dbfile")
+
+	dm, err := disk.OpenDiskManager(path, storage.PageSize)
+	if err != nil {
+		t.Fatalf("TestMultiplePageSingleFile: Open Disk Manager error: %v", err)
+	}
+
+	defer dm.Close()
+
+	pageCount := 3
+	userCounter := 1
+
+	for p := range pageCount {
+
+		rows := []Row{}
+
+		rows = append(rows, Row{
+			ID:       uint64((p+1)*10 + 1),
+			Username: fmt.Sprintf("User %d", userCounter),
+			Password: "Password1"},
+		)
+		rows = append(rows, Row{
+			ID:       uint64((p+1)*10 + 2),
+			Username: fmt.Sprintf("User %d", userCounter+1),
+			Password: "Password2"},
+		)
+		rows = append(rows, Row{
+			ID:       uint64((p+1)*10 + 3),
+			Username: fmt.Sprintf("User %d", userCounter+2),
+			Password: "Password3"},
+		)
+
+		userCounter += len(rows)
+
+		page := makeTestPage(rows)
+		pid, err := dm.AllocatePage()
+		if err != nil {
+			t.Fatalf("TestMultiplePageSingleFile: Allocate error: %v", err)
+		}
+
+		page.Header.PageID = uint64(pid)
+
+		buf, err := page.Serialize()
+
+		if err != nil {
+			t.Fatalf("TestMultiplePageSingleFile: Serialize error: %v", err)
+		}
+
+		err = dm.WritePage(pid, buf)
+		if err != nil {
+			t.Fatalf("TestMultiplePageSingleFile: Write Page error: %v", err)
+		}
+
+	}
+
+	err = dm.Sync()
+	if err != nil {
+		t.Fatalf("TestMultiplePageSingleFile: Sync error: %v", err)
+	}
+
+	//Read file
+	userCounter = 1
+	for pid := uint64(0); pid < uint64(pageCount); pid++ {
+		buf := make([]byte, storage.PageSize)
+
+		err = dm.ReadPage(disk.PageID(pid), buf)
+		if err != nil {
+			t.Fatalf("TestMultiplePageSingleFile: Read Page error: %v", err)
+		}
+
+		page, err := storage.Deserialize(buf)
+		if err != nil {
+			t.Fatalf("TestMultiplePageSingleFile: Deserialize error: %v", err)
+		}
+
+		expectedRows := []Row{
+			{ID: uint64((pid+1)*10 + 1), Username: fmt.Sprintf("User %d", userCounter), Password: "Password1"},
+			{ID: uint64((pid+1)*10 + 2), Username: fmt.Sprintf("User %d", userCounter+1), Password: "Password2"},
+			{ID: uint64((pid+1)*10 + 3), Username: fmt.Sprintf("User %d", userCounter+2), Password: "Password3"},
+		}
+		userCounter += len(expectedRows)
+
+		rowStartOffset := 0
+		rowEndOffset := RowSize
+
+		actualRows := []Row{}
+
+		for row := 0; row < len(expectedRows); row++ {
+			actualRows = append(actualRows, decodeRow(page.Data[rowStartOffset:rowEndOffset]))
+			rowStartOffset = rowEndOffset
+			rowEndOffset += RowSize
+
+		}
+
+		for count := 0; count < len(expectedRows); count++ {
+			if expectedRows[count] != actualRows[count] {
+				t.Fatalf("TestMultiplePageSingleFile: Validation test error:\nExpected: %v\nGot: %v", expectedRows[count], actualRows[count])
+			}
+		}
+
+	}
+
+}
+
+func TestMultiplePageMultipleFiles(t *testing.T) {
+	dir := t.TempDir()
+
+	for f := 0; f < 2; f++ {
+		path := filepath.Join(dir, fmt.Sprintf("dbfile_%d", f))
+
+		dm, err := disk.OpenDiskManager(path, storage.PageSize)
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Open Disk Manager error: %v", err)
+		}
+
+		rows := []Row{
+			{ID: uint64(f*100 + 1), Username: "root", Password: "RootP@ssword"},
+		}
+
+		page := makeTestPage(rows)
+		pid, err := dm.AllocatePage()
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Allocate error: %v", err)
+		}
+		page.Header.PageID = uint64(pid)
+
+		buf, err := page.Serialize()
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Serialize error: %v", err)
+		}
+
+		err = dm.WritePage(pid, buf)
+		if err != nil {
+
+			t.Fatalf("TestMultiplePageMultipleFiles: Write Page error: %v", err)
+		}
+
+		err = dm.Sync()
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Sync error: %v", err)
+		}
+
+		err = dm.Close()
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Close error: %v", err)
+		}
+
+		//Reopen file
+
+		dm, err = disk.OpenDiskManager(path, storage.PageSize)
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Open Disk Manager error: %v", err)
+		}
+
+		readBuf := make([]byte, storage.PageSize)
+		err = dm.ReadPage(pid, readBuf)
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Read Page error: %v", err)
+		}
+
+		readPage, err := storage.Deserialize(readBuf)
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Deserialize error: %v", err)
+		}
+
+		row := decodeRow(readPage.Data[:RowSize])
+
+		if rows[0] != row {
+			t.Fatalf("TestMultiplePageMultipleFiles: Decode Row error:\nExpected: %v\nGot: %v", rows[0], row)
+		}
+
+		err = dm.Close()
+		if err != nil {
+			t.Fatalf("TestMultiplePageMultipleFiles: Close error: %v", err)
+		}
 	}
 
 }
