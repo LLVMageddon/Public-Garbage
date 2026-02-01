842c362dd19b3f93f540cee7f2ef658bbd089379
Andre <144938604+LLVMageddon@users.noreply.github.com>
2026-02-01 18:03:31 +0200

    added compacting to slotted pages

--- projects/DB/src/internal/storage/slot.go
+++ projects/DB/src/internal/storage/slot.go
@@ -1,6 +1,5 @@
 package storage
 
-// TODO : Compact free space
 import (
 	"encoding/binary"
 	"errors"
@@ -15,7 +14,6 @@ const (
 	OffsetOffset = 2
 	OffsetLenght = 2
 	SlotSize     = OffsetOffset + OffsetLenght
-	// DataOffset =
 )
 
 var (
@@ -26,13 +24,10 @@ var (
 
 type SlottedPage struct {
 	Header PageHeader
-	// Slots  []Slot //FYI this is a waste of space slots can be found based on slotsize
 	Data []byte
 }
 
 func (p *SlottedPage) numSlots() int {
-	// return int(p.lower) / SlotSize
-	// return len(p.Slots)
 	return int(p.Header.SlotCount)
 }
 
@@ -65,52 +60,86 @@ func NewSlottedPage(pageID uint64) *SlottedPage {
 	return slottedPage
 }
 
+func (p *SlottedPage) compact() {
+
+	newUpper := uint16(len(p.Data))
+	newSlotId := 0
+
+	for id := 0; id < p.numSlots(); id++ {
+		slot := p.readSlot(uint16(id))
+		if slot.Length == 0 {
+			continue
+		}
+		newUpper -= slot.Length
+
+		copy(
+			p.Data[newUpper:newUpper+slot.Length],
+			p.Data[slot.Offset:slot.Offset+slot.Length],
+			)
+
+		slot.Offset = newUpper
+		p.writeSlot(uint16(newSlotId), slot)
+		newSlotId += 1
+
+	}
+	p.Header.FreeEnd = newUpper
+	p.Header.SlotCount = uint16(newSlotId)
+	p.Header.FreeStart = PageHeaderSize + (p.Header.SlotCount * SlotSize)
+}
+
 func (p *SlottedPage) Insert(record []byte) (uint16, error) {
 	recordSize := uint16(len(record))
 	slotSize := uint16(binary.Size(Slot{}))
 	prevSlot := Slot{
 		Offset: PageSize,
-		Length: 0,
 	}
 
 	for id := 0; id < p.numSlots(); id++ {
-		s := p.readSlot(uint16(id))
-		if prevSlot.Offset-s.Offset >= recordSize && s.Length == 0 {
-			copy(p.Data[s.Offset:], record)
+		readSlot := p.readSlot(uint16(id))
+		if prevSlot.Offset-readSlot.Offset >= recordSize && readSlot.Length == 0 {
+			copy(p.Data[readSlot.Offset:], record)
 			slot := Slot{
-				Offset: s.Offset,
+				Offset: readSlot.Offset,
 				// Length: s.Length,
 				Length: recordSize,
 			}
 			p.writeSlot(uint16(id), slot)
 			return uint16(id), nil
 		}
-		prevSlot = s
+		prevSlot = readSlot
 	}
 
 	available := p.Header.FreeEnd - p.Header.FreeStart
-	if available < recordSize+slotSize {
-		return 0, ErrNotSize
+	if recordSize < available {
+		p.Header.FreeEnd -= recordSize
+		copy(p.Data[p.Header.FreeEnd:], record)
+
+		slot := Slot{
+			Offset: p.Header.FreeEnd,
+			Length: recordSize,
+		}
+
+		slotOffset := p.Header.FreeStart
+		binary.LittleEndian.PutUint16(p.Data[slotOffset:], slot.Offset)
+		binary.LittleEndian.PutUint16(p.Data[slotOffset+2:], slot.Length)
+
+		p.Header.FreeStart += slotSize
+		slotID := p.Header.SlotCount
+		p.Header.SlotCount++
+
+		return slotID, nil
 	}
 
-	p.Header.FreeEnd -= recordSize
-	copy(p.Data[p.Header.FreeEnd:], record)
-
-	slot := Slot{
-		Offset: p.Header.FreeEnd,
-		Length: recordSize,
+	if recordSize > available {
+		p.compact()
+		if recordSize > (p.Header.FreeEnd - p.Header.FreeStart) {
+			return 0, errors.New("page full")
+		}
+		sloteID, error := p.Insert(record)
+		return sloteID, error
 	}
 
-	slotOffset := p.Header.FreeStart
-	binary.LittleEndian.PutUint16(p.Data[slotOffset:], slot.Offset)
-	binary.LittleEndian.PutUint16(p.Data[slotOffset+2:], slot.Length)
-
-	p.Header.FreeStart += slotSize
-	slotID := p.Header.SlotCount
-	p.Header.SlotCount++
-
-	return slotID, nil
-
+	return 0, errors.New("page full")
 }
 
 func (p *SlottedPage) Get(slotID uint16) ([]byte, error) {
--- projects/DB/src/internal/storage/slot_test.go
+++ projects/DB/src/internal/storage/slot_test.go
@@ -1,6 +1,8 @@
 package storage
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSlottedPageInsertRead(t *testing.T) {
 	p := NewSlottedPage(1)
@@ -106,6 +108,71 @@ func TestSlottedPageReclaim(t *testing.T) {
 	}
 	if string(data) != "Chewable Code" {
 		t.Fatalf("expected hllo, got %s", data)
+	}
+	// p.compact2()
+}
+
+func TestTest(t *testing.T) {
+
+	p := NewSlottedPage(1)
+	p.Insert([]byte("1, Alice, Band"))
+	p.Insert([]byte("2, Robert, Marley"))
+	p.Insert([]byte("3, Beate, Becker"))
+	p.Delete(1)
+	p.Insert([]byte("4, Daikia, Sato"))
+	p.Delete(1)
+	p.Insert([]byte("Chewable Code"))
+	p.Delete(3)
+	p.Insert([]byte("More Chewable Code"))
+	const pageSize = 4096
+	page := make([]byte, pageSize)
+
+	id, err := p.Insert(page)
+	id += 0
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.Insert([]byte("4, Daikia, Sato"))
+
+	data, err := p.Get(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != "1, Alice, Band" {
+		t.Fatalf("expected {1, Alice, Band}, got {%s}", data)
+	}
+	data, err = p.Get(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != "Chewable Code" {
+		t.Fatalf("expected {Chewable Code}, got {%s}", data)
+	}
+	data, err = p.Get(2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != "3, Beate, Becker" {
+		t.Fatalf("expected {3, Beate, Becker}, got {%s}", data)
+	}
+	data, err = p.Get(3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != "More Chewable Code" {
+		t.Fatalf("expected {More Chewable Code}, got {%s}", data)
+	}
+	err = p.Delete(4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.compact()
+	data, err = p.Get(4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != "4, Daikia, Sato" {
+		t.Fatalf("expected {4, Daikia, Sato}, got {%s}", data)
 	}
 
 }
